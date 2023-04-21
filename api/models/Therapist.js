eff5fcb2480d03e0c9b7f1bf0648637d47caedad
Andreas Wagner <awagner90@outlook.de>
2023-04-21 11:46:42 +0200

    Models erweitert

--- api/models/Therapist.js
+++ api/models/Therapist.js
@@ -3,7 +3,7 @@ module.exports = {
         name: { type: 'string', columnType: 'varchar(50)', required: true },
         firstname: { type: 'string', columnType: 'varchar(50)', required: true },
         isAdmin: { type: 'boolean', columnType: 'boolean', required: true },
-        practice: { model:'practice', required: true },
-        specialisation: {model:'specialisation'}
+        practice: { model: 'practice', required: true },
+        specialisation: { model: 'specialisation', required: true }
     },
 }
