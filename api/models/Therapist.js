49a6c85047f3254a2beb5ac51839c2de56337a77
Andreas Wagner <awagner90@outlook.de>
2023-04-22 08:16:36 +0200

    Bug Admin-Checkbox gefixed

--- api/models/Therapist.js
+++ api/models/Therapist.js
@@ -2,7 +2,7 @@ module.exports = {
     attributes: {
         name: { type: 'string', columnType: 'varchar(50)', required: true },
         firstname: { type: 'string', columnType: 'varchar(50)', required: true },
-        isAdmin: { type: 'boolean', columnType: 'boolean', required: true },
+        isAdmin: { type: 'boolean', columnType: 'boolean', required: false },
         practice: { model: 'practice', required: true },
         specialisation: { model: 'specialisation', required: true }
     },
