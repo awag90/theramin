b22be8154df7b60beed4d49081472a0fac772d0a
fi741wun <fi741wun@htwg-konstanz.de>
2023-04-22 16:28:37 +0200

    added worktime table

--- api/controllers/WorktimeController.js
+++ api/controllers/WorktimeController.js
@@ -6,7 +6,17 @@
  */
 
 module.exports = {
-  
-
+    create: async function (req, res) {
+        sails.log.debug("Creating worktime...")
+        let params = req.allParams()
+        let worktime = await Worktime.create(params).fetch()
+       // res.redirect('/practice/' + therapist.practice + '/admin')
+    },
+    edit: async function (req, res) {
+        sails.log.debug("Edit worktimes for therapist...")
+        let worktime = await Therapist.findOne({ id: req.params.id }).populate('worktime');
+        let sWorktime = await Worktime.find();
+        res.view('pages/therapist/edit', { therapist: therapist, worktime:worktime })
+    },
 };
 
