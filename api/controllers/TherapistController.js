9d72e1ff6b4cc19de488c6ac7054cc65a8c89cee
Andreas Wagner <awagner90@outlook.de>
2023-06-28 14:44:00 +0200

    Bugfix: Therapeut löschen 3

--- api/controllers/TherapistController.js
+++ api/controllers/TherapistController.js
@@ -7,65 +7,65 @@
 const Sails = require("sails/lib/app/Sails");
 
 module.exports = {
-
   create: async function (req, res) {
     try {
-    sails.log.debug("Creating therapist...");
-    let params = req.allParams();
-    let isAdmin = params.isAdmin !== undefined;
-    let newEmailAddress = params.emailAddress.toLowerCase();
-    let hashed = await sails.helpers.passwords.hashPassword(params.password);
+      sails.log.debug("Creating therapist...");
+      let params = req.allParams();
+      let isAdmin = params.isAdmin !== undefined;
+      let newEmailAddress = params.emailAddress.toLowerCase();
+      let hashed = await sails.helpers.passwords.hashPassword(params.password);
 
-    let newUserRecord = await User.create(
-      _.extend(
-        {
-          isTherapist: true,
-          isPracticeAdmin: isAdmin,
-          emailAddress: newEmailAddress,
-          password: hashed,
-        },
-        sails.config.custom.verifyEmailAddresses
-          ? {
-            emailProofToken: await Sails.helpers.strings.random(
-              "url-friendly"
-            ),
-            emailProofTokenExpiresAt:
-              Date.now() + sails.config.custom.emailProofTokenTTL,
-            emailStatus: "unconfirmed",
-          }
-          : {}
+      let newUserRecord = await User.create(
+        _.extend(
+          {
+            isTherapist: true,
+            isPracticeAdmin: isAdmin,
+            emailAddress: newEmailAddress,
+            password: hashed,
+          },
+          sails.config.custom.verifyEmailAddresses
+            ? {
+                emailProofToken: await Sails.helpers.strings.random(
+                  "url-friendly"
+                ),
+                emailProofTokenExpiresAt:
+                  Date.now() + sails.config.custom.emailProofTokenTTL,
+                emailStatus: "unconfirmed",
+              }
+            : {}
+        )
       )
-    )
-      .intercept("E_UNIQUE", "emailAlreadyInUse")
-      .intercept({ name: "UsageError" }, "invalid")
-      .fetch();
+        .intercept("E_UNIQUE", "emailAlreadyInUse")
+        .intercept({ name: "UsageError" }, "invalid")
+        .fetch();
 
-    let therapist = await Therapist.create({
-      specialisation: params.specialisation,
-      practice: params.practice,
-      user: newUserRecord.id,
-      name: params.name,
-      firstname: params.firstname,
-    }).fetch();
+      let therapist = await Therapist.create({
+        specialisation: params.specialisation,
+        practice: params.practice,
+        user: newUserRecord.id,
+        name: params.name,
+        firstname: params.firstname,
+      }).fetch();
 
-    if (!req.session.userId) {
-      req.session.userId = newUserRecord.id;
+      if (!req.session.userId) {
+        req.session.userId = newUserRecord.id;
+      }
+      res.redirect("/practice/admin");
+    } catch (err) {
+      if (err.code === "E_UNIQUE") {
+        // E-Mail-Adresse bereits vorhanden
+        return res.view("createTherapist", {
+          error: "Die E-Mail-Adresse ist bereits in Verwendung.",
+        });
+      } else if (err.name === "UsageError") {
+        // Ungültige Eingaben
+        return res.view("createTherapist", { error: "Ungültige Eingaben." });
+      } else {
+        // Andere Fehler
+        return res.serverError(err);
+      }
     }
-    res.redirect("/practice/admin");
-  } catch (err){
-    if (err.code === "E_UNIQUE" ) {
-      // E-Mail-Adresse bereits vorhanden
-      return res.view("createTherapist", { error: "Die E-Mail-Adresse ist bereits in Verwendung." });
-    } else if (err.name === "UsageError") {
-      // Ungültige Eingaben
-      return res.view("createTherapist", { error: "Ungültige Eingaben." });
-    } else {
-      // Andere Fehler
-      return res.serverError(err);
-    }
-
-  }
-},
+  },
 
   edit: async function (req, res) {
     sails.log.debug("Opening Edit-Site for therapist...");
@@ -94,18 +94,18 @@ module.exports = {
       emailAddress: newEmailAddress,
       isPracticeAdmin: isAdmin,
     });
-    if (user.id = req.session.userId) {
-      res.redirect("/therapist/overview")
+    if ((user.id = req.session.userId)) {
+      res.redirect("/therapist/overview");
     } else {
-      res.redirect("/practice/admin")
+      res.redirect("/practice/admin");
     }
   },
 
   destroy: async function (req, res) {
     sails.log.debug("Deleting therapist...");
-    let worktimes = await worktimes.destroy({therapist: req.params.id})
+    let worktimes = await Worktime.destroy({ therapist: req.params.id });
     let therapist = await Therapist.destroyOne({ id: req.params.id });
-    let user = await User.destroyOne({id: therapist.user})
+    let user = await User.destroyOne({ id: therapist.user });
     res.redirect("/practice/admin");
   },
 
@@ -115,6 +115,6 @@ module.exports = {
       .populate("practice")
       .populate("worktimes")
       .populate("user");
-    res.view("pages/therapist/show", { therapist: therapist })
+    res.view("pages/therapist/show", { therapist: therapist });
   },
 };
